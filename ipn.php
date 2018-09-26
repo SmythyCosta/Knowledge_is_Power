cae8fdf6620fbaddca1e90c1661e4bd86638ffb7
Smythy Costa <smythy.costa@hotmail.com>
2018-09-25 23:53:16 -0300

    IPN - if REQUEST_METHOD

--- ipn.php
+++ ipn.php
@@ -7,3 +7,10 @@
 // Require the configuration before any PHP code as the configuration controls error reporting:
 require('./includes/config.inc.php');
 // The config file also starts the session.
+
+// Check for a POST request, with a provided transaction ID:
+if (($_SERVER['REQUEST_METHOD'] === 'POST') && isset($_POST['txn_id']) && ($_POST['txn_type'] === 'web_accept') ) {
+
+} else { // This page was not requested via POST, no reason to do anything!
+	echo 'Nothing to do.';
+}

1b1b7cbb1d56da795867b6914310c382fddf9382
Smythy Costa <smythy.costa@hotmail.com>
2018-09-25 23:50:36 -0300

    IPN - include header

--- ipn.php
+++ ipn.php
@@ -3,3 +3,7 @@
 // This page handles the Instant Payment Notification communications with PayPal.
 // Most of the code comes from PayPal's documentation.
 // This script is created in Chapter 6.
+
+// Require the configuration before any PHP code as the configuration controls error reporting:
+require('./includes/config.inc.php');
+// The config file also starts the session.

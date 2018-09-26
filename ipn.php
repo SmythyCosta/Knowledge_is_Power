351f7d14e860a3aa08aa7a68a7b8c9e95a0ba748
Smythy Costa <smythy.costa@hotmail.com>
2018-09-25 23:49:19 -0300

    Instant Payment Notification communications with PayPal

---
+++ ipn.php
@@ -0,0 +1,5 @@
+<?php
+
+// This page handles the Instant Payment Notification communications with PayPal.
+// Most of the code comes from PayPal's documentation.
+// This script is created in Chapter 6.
